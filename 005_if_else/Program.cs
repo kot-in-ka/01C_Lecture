a329d821590d232c67cc7db05210e79f16ccb5e3
Ksenia <vorobeva.ksenia93@mail.ru>
2022-09-15 18:04:50 +0300

    005

---
+++ 005_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя ");
+string uzername = Console.ReadLine();
+
+if(uzername.ToLower() == "ксюша")
+{
+   Console.WriteLine("Ура! Это же Ксюша");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(uzername);
+}
